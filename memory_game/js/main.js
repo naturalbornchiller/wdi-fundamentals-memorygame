f83d907eabe1f17a86a458b6597381b9ba2cbdcf
Zen <mail@emmanuelprice.com>
2018-09-18 13:18:00 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = 'queen';
+let cardTwo = 'queen';
+let cardThree = 'king';
+let cardFour = 'king';
+
+console.log('User flipped ' + cardFour);
+console.log('User flipped ' + cardThree);
