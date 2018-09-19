93b34db05b020c6776729fb6e80f73a38ebc2b26
Zen <mail@emmanuelprice.com>
2018-09-19 12:58:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
-console.log('User flipped ' + cardFour);
-console.log('User flipped ' + cardThree);
+console.log('User flipped ' + cardsInPlay[0]);
+console.log('User flipped ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	alert(matchCheck(cardsInPlay[0], cardsInPlay[1]));
+}
+
+function matchCheck(a, b) {
+	return (a === b) ? 'You found a match!' : 'Sorry, try again.';
+}
