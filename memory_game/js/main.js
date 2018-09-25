f16d8e2d27329189ab2761855ce3787d5d582733
Zen <mail@emmanuelprice.com>
2018-09-25 12:53:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,42 @@
-let cards = ['queen', 'queen', 'king', 'king'];
+let cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
-	console.log((cardsInPlay[0] === cardsInPlay[1]) ? 
-							'You found a match!' : 
-							'Sorry, try again.');
+	if (cardsInPlay.length === 2) {
+		alert((cardsInPlay[0] === cardsInPlay[1]) ? 
+			'You found a match!' : 
+			'Sorry, try again.');
+	}
 }
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log('User flipped ' + cards[cardId]);
-	
+	console.log(cards[cardId].cardImage);
+	console.log('User flipped ' + cards[cardId].rank + ' of ' + cards[cardId].suit);
+
 	checkForMatch();
 }
 
